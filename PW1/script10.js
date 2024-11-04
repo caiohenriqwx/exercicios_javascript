11c2fc5ec8305a3da940ad84071d0edc869ae886
caiohenriqwx <163014315+caiohenriqwx@users.noreply.github.com>
2024-11-04 19:27:17 -0300

    Delete PW1 directory

--- PW1/script10.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("calculadoraeBtn").addEventListener("click", function() {
-    let inputValor = parseFloat(document.getElementById("inputValor").value);
-
-    if (!isNaN(inputValor)) {
-        let resultado = Math.exp(inputValor); 
-        document.getElementById("resultado").innerText = `f(${inputValor}) = ${resultado.toFixed(4)}`; 
-        alert("Por favor, insira um valor válido para x.");
-    }
-});
